c2b272f28616a1abf973a79e0f7fd73665cca126
andmohiko <andmohiko@gmail.com>
2025-03-31 19:47:10 +0900

    fix: favicon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -8,5 +8,19 @@ export const PageHead = (): React.ReactElement => (
       content="スマブラのビンゴカードを作成するサイトです。"
     />
     <link rel="icon" href="/favicon.ico" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
+    <link rel="canonical" href="https://bingo.smarepo.me/" />
+
+    {/* OGP */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="og:url" content="https://bingo.smarepo.me/" />
+    <meta property="og:title" content="スマンゴ - スマブラビンゴツール" />
+    <meta
+      property="og:description"
+      content="スマブラのビンゴカードを作成するサイトです。"
+    />
   </Head>
 )
